317882ae17b69ccad29e57871428df6bfbf13746
Wtemile <1844997875@qq.com>
2024-10-08 16:36:31 +0800

    01-03 revice

---
+++ 01/01-03.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if (a==1)
+    {
+        printf("%zu,%d,%d",sizeof(char),CHAR_MIN,CHAR_MAX);
+    }
+    else if (a==2)
+    {
+        printf("%zu,%d,%d",sizeof(unsigned char),0,UCHAR_MAX);
+    }
+    else if (a==3)
+    {
+        printf("%zu,%d,%d",sizeof(short),SHRT_MIN,SHRT_MAX);
+    }
+    else if (a==4)
+    {
+        printf("%zu,%d,%u",sizeof(unsigned short),0,USHRT_MAX);
+    }
+    else if (a==5)
+    {
+        printf("%zu,%d,%d",sizeof(int),INT_MIN,INT_MAX);
+    }
+    else if (a==6)
+    {
+        printf("%zu,%d,%u",sizeof(unsigned int),0,UINT_MAX);
+    }
+    else if (a==7)
+    {
+        printf("%zu,%ld,%ld",sizeof(long),LONG_MIN,LONG_MAX);
+    }
+    else if (a==8)
+    {
+        printf("%zu,%d,%lu",sizeof(unsigned long),0,ULONG_MAX);
+    }
+    else if (a==9)
+    {
+        printf("%zu,%lld,%lld",sizeof(long long),LONG_LONG_MIN,LONG_LONG_MAX);
+    }
+    else if (a==10)
+    {
+        printf("%zu,%d,%llu",sizeof(unsigned long long),0,ULONG_LONG_MAX);
+    }
+
+    return 0;
+}
